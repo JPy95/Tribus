400f92f519d31bbce9d304a9a0a3d4d081b05c6d
Tubias35 <jpalvees95@gmail.com>
2019-10-16 15:11:09 -0300

    Finalização da pagina de criação de prjeto, incluido arquivo de estilo para tal pagina.

--- projeto.php
+++ projeto.php
@@ -2,13 +2,41 @@
 <html lang="pt-br">
     <head>
         <?php include_once('elementos\head.php')?>
-        <link rel="stylesheet" type="text/css" href="style\index\index.css">
+        <link rel="stylesheet" type="text/css" href="style\projeto\projeto.css">
     </head>
     <body>
         <!-- Navbar -->
         <?php include_once('elementos\navbar.php')?>
 
         <!-- Conteudo -->
+        <div class="conteudo-projeto">
+            <div class="titulo-projeto">
+                <h1>Informações do Projeto</h1>
+                <div style="width: 100%;margin: 0 5%;align-items: center;">
+                    <span>
+                        Legal! Nos informe o tema do projeto e o curso dos alunos para que possam gerar os grupos de trabalho
+                    </span>
+                </div>
+            </div>
+            <div class="form-projeto">
+                <form action="">
+                    <div>
+                        <label for="">Tema do Projeto:</label><br>
+                        <input type="text">
+                    </div>
+                    <div class="select">
+                        <label for="">Curso:</label><br>
+                        <select name="" id="">
+                            <option value="">Selecione uma opção.</option>
+                            <option value="">Ciência da Computação</option>
+                        </select>
+                    </div>
+                    <div class="btn-projeto">
+                        <button class="btn_style-projeto">Criar Projeto</button>
+                    </div>
+                </form>
+            </div>
+        </div>
 
         <!-- Footer -->
         <?php include_once('elementos\footer.php')?>
