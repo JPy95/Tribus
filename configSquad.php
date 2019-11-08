d19ed519b06edeabc96b904b7f27208fe5423d02
Tubias35 <jpalvees95@gmail.com>
2019-11-08 16:48:58 -0300

    Teste em produção realizado. Sistema ok. Projeto finalizado.

--- configSquad.php
+++ configSquad.php
@@ -11,33 +11,35 @@
         <?php include_once('elementos/navbar.php')?>
 
         <!-- Conteudo -->
-        <h3>Perfil da Turma</h3>
-        <div class="managerSquad">
-            <div class="dashSquad">
-                <div>
-                    <h5>Áreas de Atuações</h5>
-                    <div class="areasTabela">
-                        <table>
-                          <tr>
-                            <th>Áreas de Atuações</th>
-                            <th>Alunos</th>
-                            <?php include_once('elementos/tableAtuacao.php')?>
-                          </tr>
-                        </table>
+        <div>
+            <h3>Perfil da Turma</h3>
+            <div class="managerSquad">
+                <div class="dashSquad">
+                    <div>
+                        <h5>Áreas de Atuações</h5>
+                        <div class="areasTabela">
+                            <table>
+                            <tr>
+                                <th>Áreas de Atuações</th>
+                                <th>Alunos</th>
+                                <?php include_once('elementos/tableAtuacao.php')?>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Perfil Comportamental</h5>
+                        <div class="pieGraph">
+                        <canvas class="pie"></canvas>
+                        <?php include_once('elementos/pieGraph.php')?>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <h5>Perfil Comportamental</h5>
-                    <div class="pieGraph">
-                      <canvas class="pie"></canvas>
-                      <?php include_once('elementos/pieGraph.php')?>
+                <div class='squads'>
+                    <h5>Grupos</h5>
+                    <div class="group-squad">
+                        <?php include_once('elementos/groups.php')?>
                     </div>
-                </div>
-            </div>
-            <div class='squads'>
-                <h5>Grupos</h5>
-                <div class="group-squad">
-                    <?php include_once('elementos/groups.php')?>
                 </div>
             </div>
         </div>
